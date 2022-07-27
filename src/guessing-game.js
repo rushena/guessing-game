04dd216b4db46c02d5f2421d14c1cbba23531080
Rushen Adjigeldieva <rushena2706@gmail.com>
2022-07-27 23:41:46 +0500

    feat: write Class methods

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,24 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.current = 0;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.current = Math.ceil((this.max + this.min) / 2);
+        return this.current;
     }
 
     lower() {
-
+        this.max = this.current;
     }
 
     greater() {
-
+        this.min = this.current;
     }
 }
 
